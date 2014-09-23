9aba1015a8285f9b3522258b04f03795ccf7323b
Naing Lin Aung <nainglinaung@aceplussolutions.com>
2014-09-23 15:08:26 +0630

    added select one

--- index.py
+++ index.py
@@ -3,7 +3,8 @@ import pymongo
 import json
 from bson import BSON,Code
 from bson import json_util
- 
+from bson.objectid import ObjectId 
+
 client = pymongo.MongoClient("localhost",27017)
 test = "admin"
 db = client[test]
@@ -21,10 +22,9 @@ def index(database,collection):
 def index(database,collection,id):
 
 	response.content_type = 'application/json; charset=utf8'
- 	doc = client[database][collection].find_one({"_id": ObjectId('id')})
-	result = json.dumps(list(doc), sort_keys=True, indent=4, default=json_util.default)
+ 	doc = client[database][collection].find_one({'_id': ObjectId(id)})
+	result = json.dumps(doc, sort_keys=True, indent=4, default=json_util.default)
 	return result
-
  
  
 run(host='localhost', port=8080)
